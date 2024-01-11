19520cd600e698cc382e40a71bcb7f4c4ee365d1
SoeunShin <21900395@handong.edu>
2024-01-11 15:37:32 +0900

    soeun / Quiz 페이지 UI 수정

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -169,7 +169,7 @@ const Quiz: React.FC = () => {
                                 <div>
                                     {quizData && (
                                         <button 
-                                            className={`w-80 h-80 rounded-md ${selectedValue === 'o' ? 'bg-pink-400' : 'bg-gray-200'} px-3.5 py-3.5 pr-8 mr-4 text-9xl font-bold text-black shadow-sm hover:bg-pink-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-200`}
+                                            className={`w-80 h-80 rounded-md ${selectedValue === 'o' ? 'bg-pink-400' : 'bg-gray-200'} px-3.5 py-3.5 mr-4 text-9xl font-bold text-black shadow-sm hover:bg-pink-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-200`}
                                             onClick={() => selectOX('o')} 
                                             disabled={selectedValue === 'o'}
                                         >
@@ -226,7 +226,6 @@ const Quiz: React.FC = () => {
                     boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.4)',
                 }}>
                     <span className="font-bold text-lg">Level</span>
-                    
                     <span className="block mt-3">5 포인트 미만 <span className="font-bold text-lg text-green-400 ml-3">lv.0</span> </span>
                     <span className="block mt-3">5 포인트 이상 <span className="font-bold text-lg text-yellow-500 ml-3">lv.1</span> </span>
                     <span className="block mt-3">10 포인트 이상 <span className="font-bold text-lg text-orange-500 ml-3">lv.2</span> </span>
