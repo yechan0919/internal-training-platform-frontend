4cfaedcbf96fd6da78b3ec1696f1c8b3fcb5cab8
SoeunShin <21900395@handong.edu>
2024-01-11 10:29:14 +0900

    soeun / Quiz 페이지 ui 수정

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -95,17 +95,16 @@ const Quiz: React.FC = () => {
                 <div
                     className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                     aria-hidden="true"
-                    >
-                    
-                    <div
-                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                    />
+                >
+                <div
+                    className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                />
                 </div>
-                <div className="mx-auto max-w-2xl pt-32 sm:pt-48 lg:pt-56">
+                <div className="mx-auto max-w-2xl pt-10 sm:pt-20 lg:pt-30">
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                             Quiz
@@ -157,27 +156,34 @@ const Quiz: React.FC = () => {
                                 </p>
                             )}
                             <div>
-                                <button 
-                                    className="w-80 h-80 rounded-md bg-gray-200 px-3.5 py-3.5 pr-8 mr-4 text-9xl font-bold text-black shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-400"
-                                    onClick={() => selectOX('o')} 
-                                    disabled={selectedValue === 'o'}>
-                                O
-                                </button>
-                                <button
-                                    className="w-80 h-80 rounded-md bg-gray-200 px-3.5 py-3.5 text-9xl font-bold text-black shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-400"
-                                    onClick={() => selectOX('x')} disabled={selectedValue === 'x'}>
-                                X
-                                </button>
+                                {quizData && (
+                                    <button 
+                                        className="w-80 h-80 rounded-md bg-gray-200 px-3.5 py-3.5 pr-8 mr-4 text-9xl font-bold text-black shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-400"
+                                        onClick={() => selectOX('o')} 
+                                        disabled={selectedValue === 'o'}>
+                                    O
+                                    </button>
+                                )}
+                                {quizData && (
+                                    <button
+                                        className="w-80 h-80 rounded-md bg-gray-200 px-3.5 py-3.5 text-9xl font-bold text-black shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-400"
+                                        onClick={() => selectOX('x')} disabled={selectedValue === 'x'}>
+                                    X
+                                    </button>
+                                )}
                             </div>
-
-                            <button 
-                                    className="rounded-md bg-indigo-300 px-3.5 py-3.5 mt-5 text-base font-bold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-                                    onClick={() => checkAnswer(`${selectedValue}`, `${quizData.topic}`)}>
-                                정답 확인
-                            </button>
-
                             <div>
-                                <p>선택된 값: {selectedValue}</p>
+                                {quizData && (
+                                    <button 
+                                        className="rounded-md bg-indigo-300 px-3.5 py-3.5 mt-5 text-base font-bold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                                        onClick={() => checkAnswer(`${selectedValue}`, `${quizData.topic}`)}>
+                                        정답 확인
+                                    </button>
+                                )}
+                            </div>
+                            {/* 확인용. 나중에 지울 것. */}
+                            <div>
+                                <p>선택된 값: {selectedValue}</p> 
                             </div>
                             
                         </div>
