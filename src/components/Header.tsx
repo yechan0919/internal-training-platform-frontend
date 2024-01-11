869553a985a60ae9da4d96ba4da45a6968046ba0
SoeunShin <21900395@handong.edu>
2024-01-11 15:41:09 +0900

    soeun / Header 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
             <header className="sticky inset-x-0 top-0 z-50 bg-white">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <a href="/" className="-m-1.5 p-1.5">
                             <img
                                 className="h-8 w-auto"
                                 src={poscoIcon}
